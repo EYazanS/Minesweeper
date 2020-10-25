9a3dbec85357ea8ed1d211b46d544f5e3669e04e
Yazan Salameh <e.yazan@live.com>
2020-10-25 13:15:20 +0200

    Added Player score and game over and game won

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
-#include<Windows.h>
+﻿#include<Windows.h>
 #include <iostream>
+#include <thread>
 
 char* Board = nullptr;
 char* BoardToRender = nullptr;
@@ -9,11 +10,25 @@ uint16_t ScreenHeight = 20, ScreenWidth = 120;
 uint16_t* mines;
 uint16_t* flags;
 
+int score;
+int playerX, playerY;
+bool Keys[5];
+
 using namespace std;
 
 void FillBoardNumbers();
 void FillMines(uint16_t minesCount);
+void GetInput();
+void ChangeCursorPosition(const HANDLE& consoleHandle);
+void ProcessPlayerMovement(bool& buttonHeld, int& currentBlocks);
 void DrawField(wchar_t* screen);
+void InitBoardToRender();
+void UpdateBoardToRender(int x, int y);
+void DidPlayerWin(int currentBlocks, bool& isRunning, bool& hasWon);
+
+HANDLE GetConsoleHandle();
+
+void DidThePlayerFindAMine(bool& isRunning);
 
 int main()
 {
@@ -22,12 +37,16 @@ int main()
 	flags = new uint16_t[BoardHeight * BoardWidth];
 	const uint32_t minesCount = 15;
 	mines = new uint16_t[minesCount];
+	playerX = 0;
+	playerY = 0;
+	score = 0;
 
-	HANDLE consoleHandle = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, NULL, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	HANDLE consoleHandle = GetConsoleHandle();
 
-	SetConsoleActiveScreenBuffer(consoleHandle);
-
+	int currentBlocks = 0;
+	bool hasWon = false;
 	bool isRunning = true;
+	bool buttonHeld = false;
 
 	int screenBufferSize = ScreenWidth * ScreenHeight;
 
@@ -42,18 +61,110 @@ int main()
 
 	FillBoardNumbers();
 
+	InitBoardToRender();
+
 	while (isRunning)
 	{
+		GetInput();
+
+		ProcessPlayerMovement(buttonHeld, currentBlocks);
+
+		ChangeCursorPosition(consoleHandle);
+
 		DrawField(screen);
 
 		WriteConsoleOutputCharacter(consoleHandle, screen, screenBufferSize, { 0, 0 }, &bytesWritten);
+
+		DidThePlayerFindAMine(isRunning);
+
+		DidPlayerWin(currentBlocks, isRunning, hasWon);
 	}
 
 	CloseHandle(consoleHandle);
 
-	cout << "Game over!! :(, your score: " << endl;
+	cout << "Game over!";
+
+	if (hasWon)
+		cout << " You Win!!";
+
+	cout << ", your score: " << score << endl;
 
 	return 0;
+}
+
+void DidThePlayerFindAMine(bool& isRunning)
+{
+	if (Keys[4] && BoardToRender[playerY * BoardWidth + playerX] == 'X')
+	{
+		this_thread::sleep_for(1000ms);
+		isRunning = false;
+	}
+}
+
+void DidPlayerWin(int currentBlocks, bool& isRunning, bool& hasWon)
+{
+	if (currentBlocks == (BoardWidth * BoardHeight) - 20)
+	{
+		this_thread::sleep_for(1000ms);
+		isRunning = false;
+		hasWon = true;
+	}
+}
+
+HANDLE GetConsoleHandle()
+{
+	HANDLE consoleHandle = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, NULL, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(consoleHandle);
+	return consoleHandle;
+}
+
+void ProcessPlayerMovement(bool& buttonHeld, int& currentBlocks)
+{
+	// Did the player press the left arrow, and can we move left?
+	playerX -= !buttonHeld && Keys[0] && (playerX - 1) >= 0;
+
+	// Did the player press the up arrow, and can we move up?
+	playerY -= !buttonHeld && Keys[1] && (playerY - 1) >= 0;
+
+	// Did the player press the right arrow, and can we move right?
+	playerX += !buttonHeld && Keys[2] && (playerX + 1) < BoardWidth;
+
+	// Did the !buttonHeld press the down arrow, and can we move down?
+	playerY += !buttonHeld && Keys[3] && (playerY + 1) < BoardHeight;
+
+	// Did the playe press space
+	if (!buttonHeld && Keys[4])
+	{
+		UpdateBoardToRender(playerX, playerY);
+		score += 10;
+		currentBlocks++;
+	}
+
+	// Because its fast, we have to prevent constent holding
+	bool anyHeld = false;
+
+	for (size_t keyIndex = 0; keyIndex < 5; keyIndex++)
+		if (Keys[keyIndex])
+			anyHeld = Keys[keyIndex];
+
+	buttonHeld = anyHeld;
+}
+
+void ChangeCursorPosition(const HANDLE& consoleHandle)
+{
+	COORD pos = { playerX, playerY };
+
+	SetConsoleCursorPosition(consoleHandle, pos);
+}
+
+void GetInput()
+{
+	// Input
+	for (size_t keyIndex = 0; keyIndex < 5; keyIndex++)
+	{
+		// Left arrow virtual key code  // Top arrow virtual key code // Right arrow virtual key code// Down arrow virtual key code // Z virtual key code key
+		Keys[keyIndex] = (0x8000 & GetAsyncKeyState((unsigned char)("\x25\x26\x27\x28\x20"[keyIndex]))) != 0;
+	}
 }
 
 
@@ -134,9 +245,21 @@ void FillBoardNumbers()
 	}
 }
 
+void InitBoardToRender()
+{
+	for (int x = 0; x < BoardWidth; x++)
+		for (int y = 0; y < BoardHeight; y++)
+			BoardToRender[y * BoardWidth + x] = '?';
+}
+
+void UpdateBoardToRender(int x, int y)
+{
+	BoardToRender[y * BoardWidth + x] = Board[y * BoardWidth + x];
+}
+
 void DrawField(wchar_t* screen)
 {
 	for (int x = 0; x < BoardWidth; x++)
 		for (int y = 0; y < BoardHeight; y++)
-			screen[y * ScreenWidth + x] = Board[y * BoardWidth + x];
+			screen[y * ScreenWidth + x] = BoardToRender[y * BoardWidth + x];
 }
